773e9f6c814c19cfce9eb97874c65147e438ee10
Antonio Alvarado <ant.36alv@gmail.com>
2020-03-29 19:07:33 -0400

    Reverted server.js to include dynamic mongoose connection.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 
 const mongoose = require("mongoose")
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true})
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true})
 
 const PORT = process.env.PORT || 3000
 
