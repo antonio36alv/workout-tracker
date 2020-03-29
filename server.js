651631fd3515b877c32a7432c580ddfcc24166c2
Antonio Alvarado <ant.36alv@gmail.com>
2020-03-29 19:05:13 -0400

    Dynamic port was added

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express")
 const app = express()
 
 const mongoose = require("mongoose")
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true})
 
-const PORT = 3000
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true})
+
+const PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
